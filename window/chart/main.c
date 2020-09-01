ba756cf6ef0875a07573edd696291aebbab2e269
MinuteReversal <114233763@qq.com>
2020-09-01 22:01:48 +0800

    add gdi

---
+++ window/chart/main.c
@@ -0,0 +1,81 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+#include <Windows.h>
+
+void onPaint(HDC hdc)
+{
+    HPEN hPen = CreatePen(PS_DOT, 1, RGB(255, 0, 0));
+    SelectObject(hdc, hPen);
+    MoveToEx(hdc, 10, 10, (LPPOINT)NULL);
+    LineTo(hdc, 200, 200);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+//https://docs.microsoft.com/en-us/windows/win32/api/wingdi/
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    wchar_t const *CLASS_NAME = L"Sample Window Class";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+    HWND hwnd = CreateWindowEx(
+        0,                   // Optional window styles.
+        CLASS_NAME,          // Window class
+        L"图表",             // Window text
+        WS_OVERLAPPEDWINDOW, // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,      // Parent window
+        NULL,      // Menu
+        hInstance, // Instance handle
+        NULL       // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        onPaint(hdc);
+        EndPaint(hwnd, &ps);
+    }
+        return 0;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
