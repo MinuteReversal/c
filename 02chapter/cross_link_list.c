2f30ffd76e377c8e4c2e8c0999fce9b2a556e3b3
MinuteReversal <114233763@qq.com>
2020-09-06 17:02:57 +0800

    trinode

---
+++ 02chapter/cross_link_list.c
@@ -0,0 +1,136 @@
+/******************************************************************************
+* fileName    : trinode.c 
+* author      : 114233763@qq.com
+* date        : 2020-09-06 15:57:01
+* description : 稀疏矩阵--十字链表
+非零元素多于零元素
+***************************************************************************** */
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct _TriNode
+{
+    //行号
+    int row;
+    //列号
+    int column;
+    //值
+    int value;
+} CrossLinkNode, *CrossLinkNode;
+
+typedef struct _Matrix
+{
+    int rows, columns;
+    int *table;
+} Matrix, *PMatrix;
+
+/**
+* 初始化矩阵
+* @param row 行总数
+* @param column 列总数
+* @return 矩阵
+*/
+PMatrix initMatix(int rows, int columns)
+{
+    const size_t intSize = sizeof(int);
+    const size_t cells = rows * columns;
+    PMatrix matrix = (PMatrix)malloc(sizeof(Matrix));
+    matrix->rows = rows;
+    matrix->columns = columns;
+    matrix->table = (int *)malloc(intSize * cells);
+
+    for (size_t row = 0; row < rows; row++)
+    {
+        for (size_t column = 0; column < columns; column++)
+        {
+            int *cell = &matrix->table[row * columns + column];
+            if (column <= row)
+            {
+                *cell = row * columns + column + 1;
+            }
+            else
+            {
+                *cell = 0;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+void printMatrix(PMatrix matrix)
+{
+    for (size_t i = 0; i < matrix->rows; i++)
+    {
+        for (size_t j = 0; j < matrix->columns; j++)
+        {
+            printf("%5d,", *(matrix->table + i * matrix->columns + j));
+        }
+        printf("\n");
+    }
+}
+
+/**
+* 释放矩阵
+* @param matrix 矩阵 
+* @return 无
+*/
+void disposeMatrix(PMatrix matrix)
+{
+    free(matrix->table);
+    free(matrix);
+}
+
+/**
+* 保存下三角矩阵
+* @param matrix 矩阵
+* @param count 数据长度
+* @return 数组
+*/
+CrossLinkNode saveToTriNodeArray(PMatrix matrix, int *count)
+{
+    //n(n+1)/2;n为行总数
+    const size_t length = matrix->rows * (matrix->rows + 1) / 2; //非零元素个数
+    CrossLinkNode a = (CrossLinkNode)malloc(sizeof(CrossLinkNode) * length);     //创建一个数组
+    int triangleIndex = 0;
+    for (size_t matrixIndex = 0; matrixIndex < matrix->rows * matrix->columns; matrixIndex++)
+    {
+        const int value = *(matrix->table + matrixIndex);
+        if (0 != value)
+        {
+            CrossLinkNode triNode = (a + triangleIndex++);
+            triNode->row = matrixIndex / matrix->columns;
+            triNode->column = matrixIndex - triNode->row * matrix->columns;
+            triNode->value = value;
+        }
+    }
+    *count = length;
+    return a;
+}
+
+/**
+* 打印一维数组
+* @param array 数组 
+* @param lenght 数组长度
+* @return 无
+*/
+void printTriNodeArray(CrossLinkNode triNode, int length)
+{
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("(%d,%d,%d),", triNode[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    PMatrix matrix = initMatix(5, 5);
+    printMatrix(matrix);
+
+    int lenght;
+    CrossLinkNode a = saveToTriNodeArray(matrix, &lenght);
+    printTriNodeArray(a, lenght);
+
+    return 0;
+}
