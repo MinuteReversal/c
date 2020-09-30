a1dd0f35a6e8cb75da71bb6b365203480cf04771
MinuteReversal <114233763@qq.com>
2020-10-01 07:46:28 +0800

    dijstra

--- 06chapter_graph/dijkstra.c
+++ 06chapter_graph/dijkstra.c
@@ -4,51 +4,110 @@
 * date        : 2020-09-27 11:39:54
 * description : 最短路径-迪杰斯特拉
 https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
 ***************************************************************************** */
 #include <stdio.h>
+#include <malloc.h>
 #include <limits.h>
-#define MAX_VEX 6
-#define Q_MAX_LEN MAX_VEX *MAX_VEX
-#include "../03chapter_stack_queue/sequence_queue.h"
+#define V 6
 #define MX INT_MAX
 
-void printArray(int *array)
+/**
+ * 获取没有搜索过的最近距离顶点索引
+ * @param dist 最近顶点
+ * @param sptSet 已搜索过的标记
+ * @return 最近顶点索引
+ */
+int minDistance(int dist[], int sptSet[])
 {
-    for (size_t i = 0; i < MAX_VEX; i++)
-    {
-        printf("%d,", array[i]);
-    }
-    printf("\n");
+    int min = INT_MAX, minIndex;
+
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == 0 && dist[v] <= min)
+            min = dist[v], minIndex = v;
+
+    return minIndex;
 }
 
-void dijkstra(int graph[][MAX_VEX], int v0)
+/**
+ * 打印最近距离
+ */
+void printSolution(int dist[])
 {
-    PQueue q = createQueue();
-    int dist[MAX_VEX];
-    for (size_t v = 0; v < Q_MAX_LEN; v++)
+    printf("Vertex \t\t Distance from Source\n");
+    for (int i = 0; i < V; i++)
+        printf("%d \t\t %d\n", i, dist[i]);
+}
+
+/**
+ * 迪杰斯特拉
+ * 单次获取最近的顶点
+ * @param graph 图
+ * @param src 起始顶点
+ * @return 返回src到最近顶点的索引与距离
+ */
+int *dijkstra(int graph[V][V], int src)
+{
+    int *dist = (int *)malloc(V * sizeof(int));
+    int sptSet[V];
+
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = 0;
+
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; count++)
     {
-        dist[v] = MX;
-        pushQueue(q, v);
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = 1;
+        for (int v = 0; v < V; v++)
+        {
+            int alt = dist[u] + graph[u][v];
+            if (!sptSet[v] && alt < dist[v])
+            {
+                dist[v] = alt;
+                printf("%d=>", u);
+            }
+        }
     }
 
-    //   for each vertex v in Graph:            
-    //       dist[v] ← INFINITY                 
-    //       prev[v] ← UNDEFINED                
-    //       add v to Q                     
-    //   dist[source] ← 0                       
-     
-    //   while Q is not empty:
-    //       u ← vertex in Q with min dist[u]   
-                                             
-    //       remove u from Q
-         
-    //       for each neighbor v of u:           // only v that are still in Q
-    //           alt ← dist[u] + length(u, v)
-    //           if alt < dist[v]:              
-    //               dist[v] ← alt
-    //               prev[v] ← u
+    return dist;
+}
 
-    //   return dist[], prev[]
+int findMinIndex(int dist[V])
+{
+    int min = -1;
+    int index = 0;
+    for (size_t i = 0; i < V; i++)
+    {
+        if (min < dist[i] && dist[i] > 0 && dist[i] < MX)
+        {
+            min = dist[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+/**
+ * 两点之间最短路径
+ * @param graph 图
+ * @param from 开始点
+ * @param to 结束点
+ * @return 路径
+ */
+int *shortPath(int graph[V][V], int from, int to)
+{
+    int next = from;
+    int path[V] = {0, 0, 0, 0, 0, 0};
+    int *dist;
+
+    while (next != to)
+    {
+        dist = dijkstra(graph, from);
+        next = findMinIndex(dist);
+    }
+    return path;
 }
 
 int main(int argc, char const *argv[])
@@ -66,7 +125,7 @@ int main(int argc, char const *argv[])
      *   (2)------>(3)<-----(5)
      *        15         3
      */
-    int matrix[MAX_VEX][MAX_VEX] = {
+    int matrix[V][V] = {
         {MX, 50, 10, MX, 45, MX},
         {MX, MX, 15, MX, 10, MX},
         {20, MX, MX, 15, MX, MX},
@@ -74,6 +133,7 @@ int main(int argc, char const *argv[])
         {MX, MX, MX, MX, MX, MX},
         {MX, MX, MX, 3, MX, MX},
     };
+
     dijkstra(matrix, 0);
     return 0;
 }
