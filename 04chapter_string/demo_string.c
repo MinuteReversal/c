da177b028eb52e103fa7663ea18fe22b92922afb
MinuteReversal <114233763@qq.com>
2020-10-18 22:36:59 +0800

    string

--- 04chapter_string/demo_string.c
+++ 04chapter_string/demo_string.c
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[])
 {
     char *s1 = "hello world!";
     char s2[10];
+    char s3[] = "how are you";
     memcpy(&s2, s1, 3);
     printf("%s\n", s2);
     return 0;
